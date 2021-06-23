1900af4131b39313ddba35bb10f914e3fae07676
Affirah <affirah.j@retailexpress.com.au>
2021-06-23 15:11:50 +1000

    first commit

---
+++ CSharpTraining/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTraining
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			//Variable declaration
+			string value;
+
+			//Assign Variable
+			value = "Good Day";
+
+			//ReadLine
+			Console.WriteLine("AJ " + value);
+			Console.WriteLine(Sum(2, 3));
+
+			//PrintLine
+			Console.ReadLine();
+		}
+		public static int Sum(int value1, int value2)
+		{
+			return value1 + value2;
+		}
+	}
+}
